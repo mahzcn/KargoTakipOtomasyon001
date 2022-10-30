5df4e2b59c804e1d0c8f5d9ee7b90c9e35142bc2
Mahzcn <mahzcn@gmail.com>
2022-10-30 17:53:15 +0300

    Giriş TC algoritması yapıldı

--- WindowsFormsApp1/Main.Designer.cs
+++ WindowsFormsApp1/Main.Designer.cs
@@ -28,24 +28,27 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
             this.btnMusteri = new System.Windows.Forms.Button();
             this.label2 = new System.Windows.Forms.Label();
             this.txtPassword = new System.Windows.Forms.TextBox();
             this.label1 = new System.Windows.Forms.Label();
-            this.mTxtSicilNo = new System.Windows.Forms.MaskedTextBox();
             this.cboxPozisyon = new System.Windows.Forms.ComboBox();
             this.label3 = new System.Windows.Forms.Label();
             this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.errorProvider1 = new System.Windows.Forms.ErrorProvider(this.components);
+            this.txtTCNo = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider1)).BeginInit();
             this.SuspendLayout();
             // 
             // btnMusteri
             // 
             this.btnMusteri.Font = new System.Drawing.Font("Noto Sans", 13.8F);
-            this.btnMusteri.Location = new System.Drawing.Point(344, 319);
+            this.btnMusteri.Location = new System.Drawing.Point(314, 267);
             this.btnMusteri.Margin = new System.Windows.Forms.Padding(5, 6, 5, 6);
             this.btnMusteri.Name = "btnMusteri";
             this.btnMusteri.Size = new System.Drawing.Size(195, 96);
-            this.btnMusteri.TabIndex = 2;
+            this.btnMusteri.TabIndex = 5;
             this.btnMusteri.Text = "Giriş";
             this.btnMusteri.UseVisualStyleBackColor = true;
             this.btnMusteri.Click += new System.EventHandler(this.btnMusteri_Click);
@@ -54,7 +57,7 @@
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Noto Sans", 13.8F);
-            this.label2.Location = new System.Drawing.Point(282, 227);
+            this.label2.Location = new System.Drawing.Point(282, 189);
             this.label2.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(76, 32);
@@ -64,33 +67,23 @@
             // txtPassword
             // 
             this.txtPassword.Font = new System.Drawing.Font("Noto Sans", 13.8F);
-            this.txtPassword.Location = new System.Drawing.Point(367, 224);
+            this.txtPassword.Location = new System.Drawing.Point(367, 186);
             this.txtPassword.Margin = new System.Windows.Forms.Padding(5, 6, 5, 6);
             this.txtPassword.Name = "txtPassword";
             this.txtPassword.PasswordChar = '*';
             this.txtPassword.Size = new System.Drawing.Size(205, 39);
-            this.txtPassword.TabIndex = 1;
+            this.txtPassword.TabIndex = 3;
             // 
             // label1
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Noto Sans", 13.8F);
-            this.label1.Location = new System.Drawing.Point(249, 129);
+            this.label1.Location = new System.Drawing.Point(273, 129);
             this.label1.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(85, 32);
             this.label1.TabIndex = 6;
             this.label1.Text = "Tc No :";
-            // 
-            // mTxtSicilNo
-            // 
-            this.mTxtSicilNo.Font = new System.Drawing.Font("Noto Sans", 13.8F);
-            this.mTxtSicilNo.Location = new System.Drawing.Point(367, 126);
-            this.mTxtSicilNo.Margin = new System.Windows.Forms.Padding(5, 6, 5, 6);
-            this.mTxtSicilNo.Mask = "00000000000";
-            this.mTxtSicilNo.Name = "mTxtSicilNo";
-            this.mTxtSicilNo.Size = new System.Drawing.Size(205, 39);
-            this.mTxtSicilNo.TabIndex = 9;
             // 
             // cboxPozisyon
             // 
@@ -101,7 +94,8 @@
             this.cboxPozisyon.Location = new System.Drawing.Point(367, 62);
             this.cboxPozisyon.Name = "cboxPozisyon";
             this.cboxPozisyon.Size = new System.Drawing.Size(205, 40);
-            this.cboxPozisyon.TabIndex = 10;
+            this.cboxPozisyon.TabIndex = 1;
+            this.cboxPozisyon.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.cboxPozisyon_KeyPress);
             // 
             // label3
             // 
@@ -117,13 +111,26 @@
             // checkBox1
             // 
             this.checkBox1.AutoSize = true;
-            this.checkBox1.Location = new System.Drawing.Point(578, 379);
+            this.checkBox1.Location = new System.Drawing.Point(565, 267);
             this.checkBox1.Name = "checkBox1";
             this.checkBox1.Size = new System.Drawing.Size(183, 36);
-            this.checkBox1.TabIndex = 12;
+            this.checkBox1.TabIndex = 4;
             this.checkBox1.Text = "Şifreyi Göster";
             this.checkBox1.UseVisualStyleBackColor = true;
             this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // errorProvider1
+            // 
+            this.errorProvider1.ContainerControl = this;
+            // 
+            // txtTCNo
+            // 
+            this.txtTCNo.Location = new System.Drawing.Point(367, 126);
+            this.txtTCNo.MaxLength = 11;
+            this.txtTCNo.Name = "txtTCNo";
+            this.txtTCNo.Size = new System.Drawing.Size(205, 39);
+            this.txtTCNo.TabIndex = 2;
+            this.txtTCNo.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox1_KeyPress);
             // 
             // Main
             // 
@@ -131,10 +138,10 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.ActiveCaption;
             this.ClientSize = new System.Drawing.Size(916, 560);
+            this.Controls.Add(this.txtTCNo);
             this.Controls.Add(this.checkBox1);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.cboxPozisyon);
-            this.Controls.Add(this.mTxtSicilNo);
             this.Controls.Add(this.btnMusteri);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.txtPassword);
@@ -144,6 +151,7 @@
             this.Name = "Main";
             this.Text = "Giriş";
             this.Load += new System.EventHandler(this.Main_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -155,10 +163,11 @@
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.TextBox txtPassword;
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.MaskedTextBox mTxtSicilNo;
         private System.Windows.Forms.ComboBox cboxPozisyon;
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.ErrorProvider errorProvider1;
+        private System.Windows.Forms.TextBox txtTCNo;
     }
 }
 
--- WindowsFormsApp1/Main.cs
+++ WindowsFormsApp1/Main.cs
@@ -8,6 +8,7 @@ using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
 
 namespace WindowsFormsApp1
 {
@@ -28,10 +29,10 @@ namespace WindowsFormsApp1
 
         private void btnMusteri_Click(object sender, EventArgs e)
         {
-            ErrorProvider provider = new ErrorProvider();
-            if (Manager.TcDogrulaV2(mTxtSicilNo.Text) == true)
+            if (txtTCNo.Text.Length ==11)
             {
-                mTxtSicilNo.BackColor = Color.White;
+
+
                 if (cboxPozisyon.Text == "Personel" || cboxPozisyon.Text == "Müdür")
 
                 {
@@ -59,12 +60,9 @@ namespace WindowsFormsApp1
 
                     }
                 }
-                else
-                {
-                    provider.SetError(cboxPozisyon, "Eksik veya yanlış girdiniz");
-                }
-
             }
+
+        }
 
 
         private void checkBox1_CheckedChanged(object sender, EventArgs e)
@@ -82,6 +80,99 @@ namespace WindowsFormsApp1
         private void Main_Load(object sender, EventArgs e)
         {
 
+        }
+
+
+        private void cboxPozisyon_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            e.Handled = true;
+        }
+
+        private void textBox1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            e.Handled = !char.IsDigit(e.KeyChar) && !char.IsControl(e.KeyChar);
+            if (e.KeyChar != (char)Keys.Back)
+            {
+
+
+                if (txtTCNo.Text.Length == 0)
+                {
+                    if (e.KeyChar == '0')
+                    {
+                        e.Handled = true;
+                        errorProvider1.SetError(txtTCNo, "İlk sayi 0 olamaz");
+
+                    }
+                    else
+                    {
+                        errorProvider1.Clear();
+                    }
+                }
+                else if (txtTCNo.Text.Length == 9)
+                {
+                    int[] dizi = new int[txtTCNo.Text.Length];
+                    for (int i = 0; i < dizi.Length; i++)
+                    {
+                        dizi[i] = (int)Char.GetNumericValue(txtTCNo.Text[i]);
+                    }
+                    int tek = 0, cift = 0;
+                    for (int i = 0; i < 9; i += 2)
+                    {
+                        tek += dizi[i];
+                    }
+
+                    for (int i = 1; i < 9; i += 2)
+                    {
+                        cift += dizi[i];
+                    }
+                    tek *= 7;
+                    int onuncuHane = (tek - cift) % 10;
+                    if (e.KeyChar.ToString() != onuncuHane.ToString())
+                    {
+                        e.Handled = true;
+                        errorProvider1.SetError(txtTCNo, "10.Hanede Hatalı giriş");
+                    }
+                    else
+                    {
+                        e.Handled = false;
+                        errorProvider1.Clear();
+                    }
+
+
+                }
+                else if (txtTCNo.Text.Length == 10)
+                {
+                    int[] dizi = new int[txtTCNo.Text.Length];
+                    for (int i = 0; i < dizi.Length; i++)
+                    {
+                        dizi[i] = (int)Char.GetNumericValue(txtTCNo.Text[i]);
+                    }
+                    int toplam = 0;
+                    for (int i = 0; i < 10; i++)
+                    {
+                        toplam += dizi[i];
+                    }
+
+
+
+                    int onbirinciHane = toplam % 10;
+                    if (e.KeyChar.ToString() != onbirinciHane.ToString())
+                    {
+                        e.Handled = true;
+                        errorProvider1.SetError(txtTCNo, "11.Hanede Hatalı giriş");
+                    }
+                    else
+                    {
+                        e.Handled = false;
+                        errorProvider1.Clear();
+                    }
+                }
+            }
+            else
+            {
+                e.Handled = false;
+                errorProvider1.Clear();
+            }
         }
     }
 }
